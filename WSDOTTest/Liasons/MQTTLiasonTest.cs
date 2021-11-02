8d0f00e0e7a0815ea6eeb0564b0316f0cd8fc0c5
Dustin Brewer <mannkind@thenullpointer.net>
2021-11-02 02:57:28 +0000

    update workflow

--- WSDOTTest/Liasons/MQTTLiasonTest.cs
+++ WSDOTTest/Liasons/MQTTLiasonTest.cs
@@ -8,15 +8,15 @@ using WSDOT.Liasons;
 using WSDOT.Models.Options;
 using WSDOT.Models.Shared;
 
-namespace WSDOTTest.Liasons
+namespace WSDOTTest.Liasons;
+
+[TestClass]
+public class MQTTLiasonTest
 {
-    [TestClass]
-    public class MQTTLiasonTest
+    [TestMethod]
+    public void MapDataTest()
     {
-        [TestMethod]
-        public void MapDataTest()
-        {
-            var tests = new[] {
+        var tests = new[] {
                 new {
                     Q = new SlugMapping { TravelTimeID = BasicTravelTimeID, Slug = BasicSlug },
                     Resource = new Resource { TravelTimeID = BasicTravelTimeID, CurrentTime = BasicTime },
@@ -29,37 +29,37 @@ namespace WSDOTTest.Liasons
                 },
             };
 
-            foreach (var test in tests)
+        foreach (var test in tests)
+        {
+            var logger = new Mock<ILogger<MQTTLiason>>();
+            var generator = new Mock<IMQTTGenerator>();
+            var sharedOpts = Options.Create(new SharedOpts
             {
-                var logger = new Mock<ILogger<MQTTLiason>>();
-                var generator = new Mock<IMQTTGenerator>();
-                var sharedOpts = Options.Create(new SharedOpts
-                {
-                    Resources = new[] { test.Q }.ToList(),
-                });
+                Resources = new[] { test.Q }.ToList(),
+            });
 
-                generator.Setup(x => x.BuildDiscovery(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<System.Reflection.AssemblyName>(), false))
-                    .Returns(new TwoMQTT.Models.MQTTDiscovery());
-                generator.Setup(x => x.StateTopic(test.Q.Slug, It.IsAny<string>()))
-                    .Returns($"totes/{test.Q.Slug}/topic/{nameof(Resource.CurrentTime)}");
+            generator.Setup(x => x.BuildDiscovery(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<System.Reflection.AssemblyName>(), false))
+                .Returns(new TwoMQTT.Models.MQTTDiscovery());
+            generator.Setup(x => x.StateTopic(test.Q.Slug, It.IsAny<string>()))
+                .Returns($"totes/{test.Q.Slug}/topic/{nameof(Resource.CurrentTime)}");
 
-                var mqttLiason = new MQTTLiason(logger.Object, generator.Object, sharedOpts);
-                var results = mqttLiason.MapData(test.Resource);
-                var actual = results.FirstOrDefault();
+            var mqttLiason = new MQTTLiason(logger.Object, generator.Object, sharedOpts);
+            var results = mqttLiason.MapData(test.Resource);
+            var actual = results.FirstOrDefault();
 
-                Assert.AreEqual(test.Expected.Found, results.Any(), "The mapping should exist if found.");
-                if (test.Expected.Found)
-                {
-                    Assert.IsTrue(actual.topic.Contains(test.Expected.Slug), "The topic should contain the expected TravelTimeID.");
-                    Assert.AreEqual(test.Expected.CurrentTime, actual.payload, "The payload be the expected CurrentTime.");
-                }
+            Assert.AreEqual(test.Expected.Found, results.Any(), "The mapping should exist if found.");
+            if (test.Expected.Found)
+            {
+                Assert.IsTrue(actual.topic.Contains(test.Expected.Slug), "The topic should contain the expected TravelTimeID.");
+                Assert.AreEqual(test.Expected.CurrentTime, actual.payload, "The payload be the expected CurrentTime.");
             }
         }
+    }
 
-        [TestMethod]
-        public void DiscoveriesTest()
-        {
-            var tests = new[] {
+    [TestMethod]
+    public void DiscoveriesTest()
+    {
+        var tests = new[] {
                 new {
                     Q = new SlugMapping { TravelTimeID = BasicTravelTimeID, Slug = BasicSlug },
                     Resource = new Resource { TravelTimeID = BasicTravelTimeID, CurrentTime = BasicTime },
@@ -67,28 +67,27 @@ namespace WSDOTTest.Liasons
                 },
             };
 
-            foreach (var test in tests)
+        foreach (var test in tests)
+        {
+            var logger = new Mock<ILogger<MQTTLiason>>();
+            var generator = new Mock<IMQTTGenerator>();
+            var sharedOpts = Options.Create(new SharedOpts
             {
-                var logger = new Mock<ILogger<MQTTLiason>>();
-                var generator = new Mock<IMQTTGenerator>();
-                var sharedOpts = Options.Create(new SharedOpts
-                {
-                    Resources = new[] { test.Q }.ToList(),
-                });
+                Resources = new[] { test.Q }.ToList(),
+            });
 
-                generator.Setup(x => x.BuildDiscovery(test.Q.Slug, It.IsAny<string>(), It.IsAny<System.Reflection.AssemblyName>(), false))
-                    .Returns(new TwoMQTT.Models.MQTTDiscovery());
+            generator.Setup(x => x.BuildDiscovery(test.Q.Slug, It.IsAny<string>(), It.IsAny<System.Reflection.AssemblyName>(), false))
+                .Returns(new TwoMQTT.Models.MQTTDiscovery());
 
-                var mqttLiason = new MQTTLiason(logger.Object, generator.Object, sharedOpts);
-                var results = mqttLiason.Discoveries();
-                var result = results.FirstOrDefault();
+            var mqttLiason = new MQTTLiason(logger.Object, generator.Object, sharedOpts);
+            var results = mqttLiason.Discoveries();
+            var result = results.FirstOrDefault();
 
-                Assert.IsNotNull(result, "A discovery should exist.");
-            }
+            Assert.IsNotNull(result, "A discovery should exist.");
         }
-
-        private static string BasicSlug = "totallyaslug";
-        private static long BasicTime = 52;
-        private static long BasicTravelTimeID = 15873525;
     }
+
+    private static string BasicSlug = "totallyaslug";
+    private static long BasicTime = 52;
+    private static long BasicTravelTimeID = 15873525;
 }
